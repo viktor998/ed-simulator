8c0bd69e3ab38f39350a43b508444c53bf709428
steplogic <kojogyaase@gmail..com>
2023-06-05 10:58:04 +0000

    update

--- src/pages/common/Header.jsx
+++ src/pages/common/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = ({ children }) => (
-  <h3 className="lg:text-[32px] text-[20px] font-semibold text-primary text-center w-full">
+  <h3 className="lg:text-[32px] text-[20px] font-semibold text-primary text-center leading-[100%] w-full">
     {children}
   </h3>
 );

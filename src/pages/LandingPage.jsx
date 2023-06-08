cf3a8cb815f00f4d584c795b63490fcafcfc9194
steplogic <kojogyaase@gmail..com>
2023-06-08 13:47:38 +0000

    update

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,53 +5,42 @@ import { useNavigate } from "react-router-dom";
 import Footer from "./common/Footer";
 import Header from "./common/Header";
 import { BritishFlag, CaImage } from "../components/Icons";
+import { EdStart } from "../components/Illustration";
 
 const LandingPage = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div className="flex flex-col items-center justify-start gap-6 py-4 max-w-[720px] mx-auto  px-[min(10%,2rem)]">
+      <div className="flex flex-col items-center justify-between h-full py-4 lg:px-0 px-3 max-w-[750px] mx-auto">
         <Header>What is your English level?</Header>
         <p className="text-primary font-[Poppins] md:text-[20px] mt-2 !leading-[99.5%] text-sm flex flex-row items-center">
           Test it now!&nbsp;
           <BritishFlag />
         </p>
-        {/* <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            flexDirection: "row",
-            width: "calc(140px +  10vw)",
-            height: "calc(200px + 10vh)",
-            margin: "auto",
-            ["@media (min-width:1024px) and (max-height:1080px)"]: {
-              height: "calc(100px + 15vh)",
-              width: "calc(90px +  10vw)",
-            },
-            ["@media (max-width:1047px)"]: {
-              width: "calc(260px + 15vw)",
-              height: "calc(100px + 12vh)",
-            },
-          }}
-        > */}
-        <CaImage className="w-8/12 md:w-full lg:w-[250px]" />
-        {/* </Box> */}
+
+        <EdStart className="w-2/5 lg:w-[316px]" />
+        <div className="text-base md:text-2xl text-center mt-4 px-4">
+          <p className="text-primary">
+            Ensure that you are situated in a tranquil setting and have ample
+            time to complete the test.
+          </p>
+          <p className="text-primary font-semibold mt-6">
+            In the event of non-completion, you will be require to restart from
+            the beginning
+          </p>
+        </div>
       </div>
       <Footer
-        prompt={
-          <>
-            It only takes <b>20 minutes</b>
-          </>
-        }
+        prompt={<></>}
         button={
           <Button
-            size="large"
             variant="contained"
             color="primary"
+            size="large"
             className="lg:max-w-[599px] max-w-[90vw] w-full"
-            onClick={() => navigate("/quiz")}
+            onClick={() => next && next()}
           >
-            GET STARTED
+            get started
           </Button>
         }
       />

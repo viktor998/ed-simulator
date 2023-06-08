f94da80837b11257ac91820ab20f605e23815f02
steplogic <kojogyaase@gmail..com>
2023-06-08 10:37:00 +0000

    update

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -8,16 +8,16 @@ function ResultsPage(props) {
     <>
       <div
         data-content
-        className="flex flex-col items-center justify-between py-4 gap-8 max-w-[720px]  px-[min(10%,2rem)] mx-auto"
+        className="flex flex-col items-center justify-between py-4 gap-8 max-w-[760px]  px-[min(10%,2rem)] mx-auto"
       >
         <Header> Great! Thanks for taking the time</Header>
         <EdFinish className="w-8/12 md:[516px] lg:w-[216px]" />
         <div className="text-base md:text-2xl text-center mt-4 flex flex-col mb-4">
           <p className="text-secondary font-semibold">
-            Your level will be assessed by our team.
+            Your level will be assessed by our team
           </p>
           <p className="text-primary px-4">
-            Go to the login and improve it when your course starts.
+            Go to the login and improve it when your course starts
           </p>
           <p className="text-primary font-semibold mt-6">
             We hope you will enjoy the course!
